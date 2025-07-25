49ad2b825a254c0b31a9b81c3a83bd860dadc2bf
DreamerLeader <88812830+DreamerLeader@users.noreply.github.com>
2025-07-25 16:29:43 +0800

    Update mla_v1.py

--- vllm_ascend/attention/mla_v1.py
+++ vllm_ascend/attention/mla_v1.py
@@ -1,5 +1,5 @@
 from dataclasses import dataclass
-from typing import TYPE_CHECKING, Any, Optional, Tuple, Type, TypeVar
+from typing import TYPE_CHECKING, Any, Optional, Tuple, Type, TypeVar, List
 
 import numpy as np
 import torch
@@ -9,6 +9,10 @@ from vllm.attention.backends.abstract import (AttentionBackend, AttentionLayer,
                                               MLAAttentionImpl)
 from vllm.attention.backends.utils import PAD_SLOT_ID
 from vllm.config import get_current_vllm_config
+from vllm.distributed.kv_transfer import (get_kv_transfer_group,
+                                          has_kv_transfer_group,
+                                          is_v1_kv_transfer_group)
+from vllm.forward_context import ForwardContext, get_forward_context
 from vllm.model_executor.layers.linear import (LinearBase,
                                                UnquantizedLinearMethod)
 from vllm.utils import cdiv, round_down
@@ -1148,6 +1152,9 @@ class AscendMLAImpl(MLAAttentionImpl):
             prefill_k_pe = k_pe[num_decode_tokens:]
         else:
             decode_hs_or_q_c = hidden_states_or_q_c
+          
+        if attn_metadata is not None:
+            self.wait_for_kv_layer_from_connector(layer.layer_name)
         if has_decode:
             decode_k_nope = None
             assert attn_metadata.decode is not None
@@ -1269,5 +1276,39 @@ class AscendMLAImpl(MLAAttentionImpl):
                     current_ms_metadata.after_comm_event.record()
             else:
                 output[:num_decode_tokens] = output_decode
+        if attn_metadata is not None:
+            self.maybe_save_kv_layer_to_connector(layer_name=layer.layer_name, kv_cache_layer=kv_cache)
 
         return output_padded
+    def wait_for_kv_layer_from_connector(self, layer_name: str):
+        if not has_kv_transfer_group() or not is_v1_kv_transfer_group():
+            return
+
+        connector = get_kv_transfer_group()
+
+        forward_context: ForwardContext = get_forward_context()
+        attn_metadata = forward_context.attn_metadata
+        if attn_metadata is None:
+            return
+        assert isinstance(attn_metadata, AscendMLAMetadata)
+        connector.wait_for_layer_load(layer_name)
+
+
+    def maybe_save_kv_layer_to_connector(
+        self,
+        layer_name: str,
+        kv_cache_layer: List[torch.Tensor],
+    ):  
+        if not has_kv_transfer_group() or not is_v1_kv_transfer_group():
+            return
+
+        connector = get_kv_transfer_group()
+
+        forward_context: ForwardContext = get_forward_context()
+        attn_metadata = forward_context.attn_metadata
+        if attn_metadata is None:
+            return
+        assert isinstance(attn_metadata, AscendMLAMetadata)
+        connector.save_kv_layer(layer_name, kv_cache_layer,
+                                attn_metadata)
+
