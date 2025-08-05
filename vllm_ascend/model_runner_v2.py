9625249f21a9d2f2ef7697aa41fc2c0b8e8e57bb
fems14 <74094523+fems14@users.noreply.github.com>
2025-08-05 14:34:06 +0800

    Update model_runner_v2.py

--- vllm_ascend/model_runner_v2.py
+++ vllm_ascend/model_runner_v2.py
@@ -2,3 +2,10 @@ ascend_config = get_ascend_config()
 
 1001
         if not self.vllm_config.model_config.use_mla:
+                
+golden_results = [
+            'Hello, my name is bioavailability裹格外 struct',
+            'The president of the United States isStr Fiona tratamientoPant narciss',
+            'The capital of France is Rocky转角){\\-Hill偷袭',
+            'The future of AI is её asegο BIOS一扫',
+        ]
